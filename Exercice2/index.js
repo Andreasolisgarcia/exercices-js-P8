9db5feab9448bb54ba4883c9042c7dc27d0ee5f1
Andreasolisgarcia <andrea.solisg@gmail.com>
2024-08-05 15:14:14 +0200

    format exo6

--- Exercice2/index.js
+++ Exercice2/index.js
@@ -1,36 +1,34 @@
 // Créez votre fonction ici
 
-function calculate(a,b, sign){
-    switch (sign){
-        case '+' : {
-            return a + b;
-        }
-        case '-': {
-            return a - b;
-        }
-        case '*': {
-            return a * b;
-        }
-        case '/': {
-            if (b == 0){
-                return "Division by zero is not allowed"
-            }
-            return a / b;
-        }
-        case '%': {
-
-                return "Invalid operator"
-
-        }
+function calculate(a, b, sign) {
+  switch (sign) {
+    case "+": {
+      return a + b;
     }
+    case "-": {
+      return a - b;
+    }
+    case "*": {
+      return a * b;
+    }
+    case "/": {
+      if (b == 0) {
+        return "Division by zero is not allowed";
+      }
+      return a / b;
+    }
+    case "%": {
+      return "Invalid operator";
+    }
+  }
 }
 
 // Exemples d'utilisation de la fonction
-console.log(calculate(5, 3, '+'));   // Affiche 8
-console.log(calculate(10, 4, '-'));  // Affiche 6
-console.log(calculate(7, 2, '*'));   // Affiche 14
-console.log(calculate(12, 3, '/'));  // Affiche 4
-console.log(calculate(8, 0, '/'));   // Affiche "Division by zero is not allowed"
-console.log(calculate(4, 5, '%'));   // Affiche "Invalid operator"
+console.log(calculate(5, 3, "+")); // Affiche 8
+console.log(calculate(10, 4, "-")); // Affiche 6
+console.log(calculate(7, 2, "*")); // Affiche 14
+console.log(calculate(12, 3, "/")); // Affiche 4
+console.log(calculate(8, 0, "/")); // Affiche "Division by zero is not allowed"
+console.log(calculate(4, 5, "%")); // Affiche "Invalid operator"
 
 // export default calculate
