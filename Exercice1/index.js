955fcaf86a37f6da31dc28912471514cdb8caf39
Andreasolisgarcia <andrea.solisg@gmail.com>
2024-08-05 12:42:59 +0200

    exo1 done

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,15 @@
 //votre code ici
 
+function pairNumbers(a,b) {
+    let pairNumbersArray = []
+
+    for (let i = a; i <= b; i++){
+        if(i%2 === 0) {
+            pairNumbersArray.push(i)
+        } 
+    }
+
+    return pairNumbersArray
+}
+
 export default pairNumbers
