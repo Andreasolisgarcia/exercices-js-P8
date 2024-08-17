5343525b6bc9d25dae06c60270ddf04313888ac6
Andreasolisgarcia <andrea.solisg@gmail.com>
2024-08-17 18:49:06 +0200

    esport default uncomment

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -25,9 +25,9 @@ function calculateAverage(arrayOfNumbers = []) {
 //     return sum / arrayOfNumbers.length
 // }
 
-// Exemples d'utilisation de la fonction
-console.log(calculateAverage([5, 10, 15])); // retourne 10
-console.log(calculateAverage([10, 20, 30, 20])); // retourne 20
-console.log(calculateAverage()); // No numbers to calculate average
+// // Exemples d'utilisation de la fonction
+// console.log(calculateAverage([5, 10, 15])); // retourne 10
+// console.log(calculateAverage([10, 20, 30, 20])); // retourne 20
+// console.log(calculateAverage()); // No numbers to calculate average
 
-// export default calculateAverage
+export default calculateAverage
